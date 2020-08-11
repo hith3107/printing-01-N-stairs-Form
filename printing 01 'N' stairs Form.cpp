9201b0636aa9d20c38a7b2a833e7857e0c6f1180
hith3107 <69384270+hith3107@users.noreply.github.com>
2020-08-11 21:11:02 +0530

    Add files via upload

---
+++ printing 01 'N' stairs Form.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+int i, j, N;
+printf("Enter N: ");
+scanf("%d", &N);
+for(i=1; i<=N; i++)
+{
+   for(j=1; j<=2*i; j++)
+     {
+        if(j % 2 == 0)
+            {
+            printf("1");
+            }
+            else
+            {
+              printf("0");
+            }
+    }       
+	 printf("\n");
+    }
+return 0;
+}
